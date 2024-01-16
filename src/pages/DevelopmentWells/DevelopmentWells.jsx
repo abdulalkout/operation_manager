a7da847cb610f2dea46f007b0fcf254b09b30307
abdulalkout <abdul.alkout@gmail.com>
2024-01-16 11:59:25 -0600

    drilling wells are displyed on frontend

--- src/pages/DevelopmentWells/DevelopmentWells.jsx
+++ src/pages/DevelopmentWells/DevelopmentWells.jsx
@@ -5,23 +5,9 @@ import * as wllsAPI from "../../utilities/wells-api";
 import DataComponentsList from "../../components/DataComponents/DataComponentsList";
 
 function DevelopmentWells({ user, setUser }) {
-  const [allWells, setAllWells] = useState([]);
-  const [productionWells, setProductionWells] = useState([]);
   const [developmentWells, setDevelopmentWells] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      try {
-        const wells = await wllsAPI.getAll();
-        setAllWells(wells);
-      } catch (error) {
-        console.error("Error fetching all wells:", error);
-      }
-      try {
-        const wells = await wllsAPI.getProductionWells();
-        setProductionWells(wells);
-      } catch (error) {
-        console.error("Error fetching production wells:", error);
-      }
       try {
         const wells = await wllsAPI.getDevelopmentWells();
         setDevelopmentWells(wells);
@@ -37,7 +23,7 @@ function DevelopmentWells({ user, setUser }) {
     <div className="allwells-div">
       <OpNavbar user={user} setUser={setUser} />
       <div>
-        <DataComponentsList allData={allWells} />
+        <DataComponentsList allData={developmentWells} />
       </div>
     </div>
   );
